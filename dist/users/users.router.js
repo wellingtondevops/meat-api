35034485c996206693678adedc991f65fb5e43d5
wellingtondevops <wellingtondevops@gmail.com>
2019-01-28 13:36:43 -0200

    Extendendo o Router para outros Recursos

--- dist/users/users.router.js
+++ dist/users/users.router.js
@@ -1,58 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const router_1 = require("../common/router");
+const model_router_1 = require("../common/model-router");
 const users_model_1 = require("./users.model");
-const restify_errors_1 = require("restify-errors");
-class UserRouter extends router_1.Router {
+class UserRouter extends model_router_1.ModelRouter {
     constructor() {
-        super();
+        super(users_model_1.User);
         this.on('beforeRender', document => {
             document.password = undefined;
         });
     }
     applyRoutes(applycation) {
-        //endpoint pesquis todos usuários;
-        applycation.get('/users', (req, resp, next) => {
-            users_model_1.User.find().then(this.render(resp, next));
-        });
-        //endpoint pesquisa usuário por id;
-        applycation.get('/users/:id', (req, resp, next) => {
-            users_model_1.User.findById(req.params.id).then(this.render(resp, next)).catch(next);
-        });
-        //new endpoint
-        applycation.post('/users', (req, resp, next) => {
-            let user = new users_model_1.User(req.body);
-            user.save().then(this.render(resp, next)).catch(next);
-        });
-        applycation.put('/users/:id', (req, resp, next) => {
-            const options = { runValidators: true, overwrite: true };
-            users_model_1.User.update({ _id: req.params.id }, req.body, options)
-                .exec().then(result => {
-                if (result.n) {
-                    return users_model_1.User.findById(req.params.id);
-                }
-                else {
-                    throw new restify_errors_1.NotFoundError('Usuário não encontrado!');
-                }
-            }).then(this.render(resp, next)).catch(next);
-        });
-        //metodo Path melhor não precisa  alterar tudo objeto
-        applycation.patch('users/:id', (req, resp, next) => {
-            const options = { runValidators: true, new: true };
-            users_model_1.User.findByIdAndUpdate(req.params.id, req.body, options).
-                then(this.render(resp, next)).catch(next);
-        });
-        applycation.del('/users/:id', (req, resp, next) => {
-            users_model_1.User.remove({ _id: req.params.id }).exec().then((cmResult) => {
-                if (cmResult.result.n) {
-                    resp.send(204);
-                }
-                else {
-                    throw new restify_errors_1.NotFoundError('Usuário não encontrado!');
-                }
-                return next();
-            }).catch(next);
-        });
+        applycation.get('/users', this.findAll);
+        applycation.get('/users/:id', [this.validateId, this.findById]);
+        applycation.post('/users', this.save);
+        applycation.put('/users/:id', [this.validateId, this.replace]);
+        applycation.patch('users/:id', [this.validateId, this.update]);
+        applycation.del('/users/:id', [this.validateId, this.delete]);
     }
 }
 exports.usersRouter = new UserRouter();
--- users/users.router.ts
+++ users/users.router.ts
@@ -1,12 +1,13 @@
-import { Router } from "../common/router";
+
+import {ModelRouter} from '../common/model-router'
 import * as restify from 'restify'
 import {User} from './users.model'
 import {NotFoundError} from 'restify-errors'
 
-class UserRouter extends Router {
+class UserRouter extends ModelRouter<User> {
 
     constructor(){
-        super()
+        super(User)
         this.on('beforeRender', document=>{
             document.password= undefined
         })
@@ -14,64 +15,19 @@ class UserRouter extends Router {
 
 
 
-    applyRoutes(applycation: restify.Server){
+    applyRoutes(applycation: restify.Server){         
 
-         //endpoint pesquis todos usuários;
+        applycation.get('/users', this.findAll)    
 
-        applycation.get('/users', (req, resp, next)=>{
+        applycation.get('/users/:id',[this.validateId,this.findById])      
 
-            User.find().then(this.render(resp, next))
-        })
+        applycation.post('/users', this.save)
 
-        //endpoint pesquisa usuário por id;
+        applycation.put('/users/:id',[this.validateId, this.replace])        
 
-        applycation.get('/users/:id', (req, resp, next)=>{
-            User.findById(req.params.id).then(this.render(resp, next)).catch(next)
-        })
-        //new endpoint
+        applycation.patch('users/:id',[this.validateId, this.update])        
 
-        applycation.post('/users', (req, resp, next)=>{
-            let user = new User(req.body)            
-            user.save().then(this.render(resp, next)).catch(next)
-        })
-
-        applycation.put('/users/:id', (req, resp, next)=>{
-            const options  = {runValidators:true, overwrite: true}
-            User.update({_id:req.params.id}, req.body,  options)
-                .exec().then(result=>{
-                  if(result.n){
-                    return User.findById(req.params.id)
-                  }else{
-                    throw new NotFoundError('Usuário não encontrado!')
-                  }
-            }).then(this.render(resp, next)).catch(next)
-          })
-
-
-          //metodo Path melhor não precisa  alterar tudo objeto
-
-        applycation.patch('users/:id', (req, resp, next)=>{
-            const options = {runValidators:true, new :true}
-
-            User.findByIdAndUpdate(req.params.id, req.body, options).
-            then(this.render(resp, next)).catch(next)
-        })
-
-        
-
-        applycation.del('/users/:id', (req, resp, next)=>{
-            User.remove({_id: req.params.id}).exec().then((cmResult: any)=>{
-                if(cmResult.result.n){
-                    resp.send(204)                    
-                }else{
-                    throw new NotFoundError('Usuário não encontrado!')
-                }
-                return next()
-            }).catch(next)
-
-        })
-
-    }
+        applycation.del('/users/:id', [this.validateId,this.delete])    }
 }
 
 export const usersRouter = new UserRouter()
